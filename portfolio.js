15348ce206ccd5c1fda50e27ecfc543a41a02434
Gonzalo Andres Diaz <gonchi.diaz125@gmail.com>
2023-12-20 15:36:57 -0300

    Primer commit - Pagina principal y CSS

---
+++ portfolio.js
@@ -0,0 +1,41 @@
+//funcion para scroll suave:
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.scroll').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+  
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+  });
+  
+  //----------------------------------------------------------------------------------------------------------------
+
+  // Código para el marcado de secciones en el Navbar al hacer scroll
+  document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    window.addEventListener('scroll', () => {
+      let current = '';
+
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+          current = section.getAttribute('id');
+        }
+      });
+
+      navLinks.forEach(navLink => {
+        navLink.classList.remove('active');
+        if (navLink.getAttribute('href').slice(1) === current) {
+          navLink.classList.add('active');
+        }
+      });
+    });
+  });
+
